ecdb6ea7709748e50fae5efc4864619bf1cb0dcc
Максим <sterh84@gmail.com>
2024-02-20 12:28:42 +0300

    Рефакторинг функции

--- js/functions.js
+++ js/functions.js
@@ -1,64 +1,32 @@
-// function stringLength(string, length){
-//   return string.length <= length;
-// }
-
 const stringLength = (string, length) => string.length <= length;
-
-// Тест функции
 
 // eslint-disable-next-line
 console.log(stringLength('Кекс', 3));
 
-function palindrome(string){
+const palindrome = (string) => {
   const newString = string.replaceAll(' ','').toLowerCase();
   let total = '';
   for (let i = newString.length - 1; i >= 0; i--){
     total += newString.at(i);
   }
   return total === newString;
-}
-
-// Тест функции
+};
 
 /* eslint-disable */
 console.log(palindrome("Топот"));
 console.log(palindrome('Лёша на полке клопа нашёл'));
 /* eslint-enable */
 
-function getNumber(string){
-  if(typeof string === 'string'){
-    let newString = '';
-    for(let i = 0; i < string.length; i++){
-      if(!Number.isNaN(parseInt(string.at(i), 10))){
-        newString += string.at(i);
-      }
+const getNumber = (str) => {
+  const stringToCheck = str.toString();
+  let newString = '';
+  for(let i = 0; i < stringToCheck.length; i++){
+    if(!Number.isNaN(parseInt(stringToCheck[i], 10))) {
+      newString += stringToCheck[i];
     }
-    if(newString === ''){
-      return NaN;
-    }
-
-    return parseInt(newString, 10);
-
-  }else if(typeof string === 'number'){
-    let newString = '';
-    const numberString = string.toString();
-    for(let i = 0; i < numberString.length; i++){
-      if(!Number.isNaN(parseInt(numberString.at(i), 10))){
-        newString += numberString.at(i);
-      }
-    }
-    if(newString === ''){
-      return NaN;
-    }
-
-    return parseInt(newString, 10);
-
-  }else{
-    return NaN;
   }
-}
-
-// Тест функции
+  return newString ? +newString : NaN;
+};
 
 /* eslint-disable */
 console.log(getNumber('2023 год')); // 2023
