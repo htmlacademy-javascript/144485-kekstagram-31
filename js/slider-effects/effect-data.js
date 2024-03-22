a46e8d1de08fafb11800b2b1ec99d13f929ba17a
Максим <sterh84@gmail.com>
2024-03-22 16:45:57 +0300

    Помощь друга

--- js/slider-effects/effect-data.js
+++ js/slider-effects/effect-data.js
@@ -10,7 +10,7 @@ export const EFFECTS = [
     max: 1,
     step: 0.1,
     start: 1,
-    unit: ' ',
+    unit: '',
   },
   {
     name: 'sepia',
@@ -19,7 +19,7 @@ export const EFFECTS = [
     max: 1,
     step: 0.1,
     start: 1,
-    unit: ' ',
+    unit: '',
   },
   {
     name: 'marvin',
@@ -46,6 +46,6 @@ export const EFFECTS = [
     max: 3,
     step: 0.1,
     start: 3,
-    unit: ' ',
+    unit: '',
   },
 ];
