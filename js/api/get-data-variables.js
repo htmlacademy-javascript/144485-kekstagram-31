23ad3e427e42eff7dee81720dc19826d405cd14a
Максим <sterh84@gmail.com>
2024-03-28 17:17:45 +0300

    поправил url

--- js/api/get-data-variables.js
+++ js/api/get-data-variables.js
@@ -2,7 +2,7 @@ export const dataErrorTemplate = document.querySelector('#data-error').content.q
 export const sendErrorTemplate = document.querySelector('#error').content.querySelector('.error');
 export const sendFormErrorTemplate = document.querySelector('#success').content.querySelector('.success');
 export const SHOW_ERROR_TIME = 5000;
-export const BASE_URL = 'https://31.javascript.htmlacademy.pro/kekstagra';
+export const BASE_URL = 'https://31.javascript.htmlacademy.pro/kekstagram';
 export const Route = {
   GET_DATA: '/data',
   SEND_DATA: '/',
